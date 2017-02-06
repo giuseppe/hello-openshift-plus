cb1bda2c536ef0519ee283aeff29ddd0b984a4f2
Giuseppe Scrivano <gscrivan@redhat.com>
2017-02-06 16:35:21 +0100

    Poor man HTML output
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,26 +9,42 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 
-	fmt.Fprintln(w, "Hello OpenShift!  These are my env variables:")
+	fmt.Fprintln(w, "<h1>Hello OpenShift!</h1><br/>\n")
+	fmt.Fprintln(w, "<html><div style='border:1px solid black; align:center; width:80%'>\n")
+
 	env := os.Environ()
 
 	for _, value := range env {
 		name := strings.Split(value, "=")
-		fmt.Fprintf(w, "%s : %s\n", name[0], name[1])
+		fmt.Fprintf(w, "%s : %s <br />", name[0], name[1])
 	}
 
-	fmt.Fprintln(w, "These are my network addresses:\n")
+	fmt.Fprintln(w, "</div>\n<p>&nbsp;</p>\n<div style='border:1px solid black; align:center; width:80%'>\n")
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		return
 	}
 
 	for _, addr := range addrs {
-		fmt.Fprintf(w, "%s\n", addr.String())
+		fmt.Fprintf(w, "%s<br />\n", addr.String())
 	}
+	fmt.Fprintln(w, "</div>\n")
+
+	fmt.Fprintln(w, "</div>\n<p>&nbsp;</p>\n<div style='border:1px solid black; align:center; width:80%'>\n")
+	ifs, err := net.Interfaces()
+	if err != nil {
+		return
+	}
+
+	for _, ifc := range ifs {
+		fmt.Fprintf(w, "%s -> %s<br />\n", ifc.Name, ifc.HardwareAddr.String())
+	}
+	fmt.Fprintln(w, "</div>\n")
+
+	fmt.Fprintln(w, "</html>\n")
 	
 	fmt.Println("Servicing request.")
 }
